6ffc58cc1e69ae574235ef35bc90d6d73b596c6a
Tom Nielsen <tanielsen@gmail.com>
2026-09-04 14:55:23 +0100

    basics

---
+++ index.js
@@ -0,0 +1,73 @@
+const {
+  input,
+  div,
+  text,
+  script,
+  domReady,
+  textarea,
+  style,
+} = require("@saltcorn/markup/tags");
+const File = require("@saltcorn/data/models/file");
+const User = require("@saltcorn/data/models/user");
+const {
+  standardConfigFields,
+  initTiny,
+  encodeAmpersands,
+} = require("./common");
+const { features } = require("@saltcorn/data/db/state");
+
+const headers = [
+  {
+    script: `/plugins/public/qr-scanner@${
+      require("./package.json").version
+    }/qr-scanner.umd.min.js`,
+  },
+];
+
+const TinyMCE = {
+  type: "HTML",
+  isEdit: true,
+  blockDisplay: true,
+  handlesTextStyle: true,
+  configFields: standardConfigFields,
+  run: (nm, v, attrs, cls, required, header) => {
+    const rndcls = `tmce${Math.floor(Math.random() * 16777215).toString(16)}`;
+    const tasklistWhites = attrs?.include_tasklist
+      ? { ul: ["class"], li: ["class"] }
+      : {};
+    return div(
+      {
+        class: [cls],
+      },
+      textarea(
+        {
+          name: text(nm),
+          id: `input${text(nm)}_${rndcls}`,
+          rows: 10,
+          class: rndcls,
+          "data-postprocess": "$e.text()",
+        },
+        text(
+          attrs?.include_drawio && attrs.diagram_format === "svg"
+            ? encodeAmpersands(v)
+            : v || "",
+          attrs?.include_drawio
+            ? { div: ["drawio-diagram", "id"], ...tasklistWhites }
+            : tasklistWhites,
+        ),
+      ),
+      script(
+        domReady(`setTimeout(async ()=>{      
+          ${initTiny(nm, rndcls, attrs, header?.in_auto_save)}
+    },0)`),
+      ),
+    );
+  },
+};
+
+module.exports = {
+  sc_plugin_api_version: 1,
+  fieldviews: { TinyMCE },
+  plugin_name: "qr-scanner",
+  headers,
+};
